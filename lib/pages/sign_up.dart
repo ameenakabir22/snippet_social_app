fd18d760bf9d458fb01b0f90d295e9a88e12aeb5
Amina_Kabir <ameenakabir22@gmail.com>
2023-11-12 21:35:22 +0000

    the sign up page is slowly coming to life

--- lib/pages/sign_up.dart
+++ lib/pages/sign_up.dart
@@ -17,53 +17,141 @@ class _SignUpState extends State<SignUp> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Sign Up ',
+                style: TextStyle(
+                    color: Colors.pink[600],
+                    fontSize: 50,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic),
+              ),
+            ),
+            SizedBox(
+              height: 80.0,
+            ),
+
             // text field for email
             TextField(
-              decoration: InputDecoration(labelText: 'Email'),
+              decoration: InputDecoration(
+                  labelText: 'Email',
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  )),
               keyboardType: TextInputType.emailAddress,
               controller: _emailController,
             ),
             SizedBox(height: 16.0),
             // text field for username
             TextField(
-              decoration: InputDecoration(labelText: 'Username'),
+              decoration: InputDecoration(
+                  labelText: 'Username',
+                  labelStyle: TextStyle(color: Colors.black),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  )),
               controller: _usernameController,
             ),
             SizedBox(height: 16.0),
             // Text field for password
             TextField(
-                controller: _passwordController,
-                decoration: InputDecoration(labelText: 'Password'),
-                obscureText: true),
+              controller: _passwordController,
+              decoration: InputDecoration(
+                  labelText: 'Password',
+                  labelStyle: TextStyle(color: Colors.black),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  )),
+              obscureText: true,
+            ),
+
             SizedBox(
               height: 16.0,
             ),
             // text field for confirm password
             TextField(
-                controller: _confirmPasswordController,
-                decoration: InputDecoration(labelText: 'Confirm-Password'),
-                obscureText: true),
+              controller: _confirmPasswordController,
+              decoration: InputDecoration(
+                  labelText: 'Confirm-Password',
+                  labelStyle: TextStyle(color: Colors.black),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(color: Colors.black),
+                  )),
+              obscureText: true,
+            ),
             SizedBox(
               height: 16.0,
             ),
             // sign up button
             ElevatedButton(
               onPressed: () {
-                // Perform sign-up logic (e.g., send data to server)
-                String username = _usernameController.text;
-                String email = _emailController.text;
-                String password = _passwordController.text;
-
+                // Perform sign-up logic
+                String username = _usernameController.text.trim();
+                String email = _emailController.text.trim();
+                String password = _passwordController.text.trim();
+                String confirmPassword = _confirmPasswordController.text.trim();
                 // Implement your sign-up logic here
+                // Check for empty fields
+                if (username.isEmpty ||
+                    email.isEmpty ||
+                    password.isEmpty ||
+                    confirmPassword.isEmpty) {
+                  // Handle empty fields
+                  print('Please fill in all fields');
+                  return;
+                }
+
+                // Check if passwords match
+                if (password != confirmPassword) {
+                  // Handle mismatched passwords
+                  print('Passwords do not match');
+                  return;
+                }
 
                 //  print the entered values
                 print('Username: $username');
                 print('Email: $email');
                 print('Password: $password');
               },
+              style: ButtonStyle(
+                backgroundColor: MaterialStateProperty.all<Color>(
+                    Colors.pink), // Background color
+                textStyle: MaterialStateProperty.all<TextStyle>(
+                  TextStyle(fontSize: 16, color: Colors.white), // Text style
+                ),
+                elevation: MaterialStateProperty.all<double>(8), // Elevation
+                side: MaterialStateProperty.all<BorderSide>(
+                  BorderSide(
+                      width: 2.0, color: Colors.black), // Border properties
+                ),
+              ),
               child: Text('Sign Up'),
             ),
           ],
